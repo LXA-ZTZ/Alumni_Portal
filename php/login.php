772c17e3370c4d093274fb550cd0249c8e9f6084
Joseph Murphy III <jmrphyii@memphis.edu>
2014-03-27 15:05:52 -0500

    login php file
    php file to login

---
+++ php/login.php
@@ -0,0 +1,22 @@
+<?php
+
+	$con = mysqli_connect('localhost', 'root', 'pass', 'db_name');
+	if(!$db){
+		echo "unable to connect to data base";	
+	}
+	
+	$user_name = $_POST[''];
+	$pass = $_POST[''];
+	
+	$query = mysqli_query($db, "SELECT * FROM users");
+	
+	if($query){
+		
+	}else{
+		
+	}
+	
+
+
+	mysqli_close($con);
+?>
