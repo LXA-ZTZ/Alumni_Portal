5a36198e027597c8c5b48884e6651c1158722376
Joseph Murphy III <jmrphyii@memphis.edu>
2014-03-26 19:01:39 -0500

    Update index.php

--- index.php
+++ index.php
@@ -40,7 +40,7 @@
 
     <div class="container">
 
-      <form class="form-signin" role="form">
+      <form class="form-signin" role="form" method="login.php" method="POST">
         <h2 class="form-signin-heading">Please sign in</h2>
         <input type="email" class="form-control" placeholder="Email address" required autofocus>
         <input type="password" class="form-control" placeholder="Password" required>
